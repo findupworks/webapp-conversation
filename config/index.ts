20318baa8cd38a75fd076f3c5b0077acb043ff0d
Erick Haendel <erick.haendel@findup.com.br>
2024-05-21 17:19:38 -0300

    Init FindUP

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,11 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
+  title: 'Aurora - Hapvida',
+  description: 'Hapvida Treinamento',
+  copyright: 'FindUP',
   privacy_policy: '',
-  default_language: 'zh-Hans',
+  default_language: 'pt-BR',
 }
 
 export const isShowPrompt = false
