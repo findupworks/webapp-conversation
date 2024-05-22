d6a07e22c68fb5113c3e2971167fe85234a4f9f9
Erick Haendel <erick.haendel@findup.com.br>
2024-05-22 14:59:06 -0300

    Ajustes

---
+++ i18n/lang/app.pt.ts
@@ -0,0 +1,34 @@
+const translation = {
+  common: {
+    welcome: 'Bem-vindo ao uso',
+    appUnavailable: 'Aplicativo indisponível',
+    appUnkonwError: 'Erro desconhecido no aplicativo',
+  },
+  chat: {
+    newChat: 'Novo chat',
+    newChatDefaultName: 'Nova conversa',
+    openingStatementTitle: 'Declaração de abertura',
+    powerBy: 'Desenvolvido por FindUP',
+    prompt: 'Prompt',
+    privatePromptConfigTitle: 'Configurações de conversa',
+    publicPromptConfigTitle: 'Prompt Inicial',
+    configStatusDes: 'Antes de começar, você pode modificar as configurações da conversa',
+    configDisabled:
+      'As configurações da sessão anterior foram usadas para esta sessão.',
+    startChat: 'Iniciar Chat',
+    privacyPolicyLeft:
+      'Por favor, leia a ',
+    privacyPolicyMiddle:
+      'política de privacidade',
+    privacyPolicyRight:
+      ' fornecida pelo desenvolvedor do aplicativo.',
+  },
+  errorMessage: {
+    valueOfVarRequired: 'O valor das variáveis não pode estar vazio',
+    waitForResponse:
+      'Por favor, aguarde a resposta da mensagem anterior ser concluída.',
+  },
+}
+
+
+export default translation
---
+++ i18n/lang/common.pt.ts
@@ -0,0 +1,33 @@
+const translation = {
+  api: {
+    success: 'Sucesso',
+    saved: 'Salvo',
+    create: 'Criado',
+  },
+  operation: {
+    confirm: 'Confirmar',
+    cancel: 'Cancelar',
+    clear: 'Limpar',
+    save: 'Salvar',
+    edit: 'Editar',
+    refresh: 'Reiniciar',
+    search: 'Buscar',
+    send: 'Enviar',
+    lineBreak: 'Quebra de linha',
+    like: 'gostar',
+    dislike: 'não gostar',
+    ok: 'OK',
+  },
+  imageUploader: {
+    uploadFromComputer: 'Carregar do Computador',
+    uploadFromComputerReadError: 'Falha na leitura da imagem, por favor tente novamente.',
+    uploadFromComputerUploadError: 'Falha no upload da imagem, por favor carregue novamente.',
+    uploadFromComputerLimit: 'As imagens carregadas não podem exceder {{size}} MB',
+    pasteImageLink: 'Colar link da imagem',
+    pasteImageLinkInputPlaceholder: 'Cole o link da imagem aqui',
+    pasteImageLinkInvalid: 'Link da imagem inválido',
+    imageUpload: 'Upload de Imagem',
+  },
+}
+
+export default translation
